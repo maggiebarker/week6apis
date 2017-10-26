234af22f0110f2e779ef05b5dcea2e870c4f4011
maggiebarker <maggieb.06@gmail.com>
2017-10-25 21:42:17 -0600

    stop/go complete

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -21,19 +21,18 @@ $("#add-sport").on("click", function(event) {
 	var sports = $("#sports-input").val().trim();
 	topics.push(sports);
 	console.log(topics);
+	$("#sports-input").empty();
 
 renderBtns();
 
 });
-
 renderBtns();
-
 
 //Bring in the gifs! First, our key:
 var apiKey = "228z5wIEwb7ZVKcEdK6NJoB2D87Mu7D7";
 
-    $("#sports-view").on("click", ".sports", function() {
-    	$("#sportsGifs").empty()
+$("#sports-view").on("click", ".sports", function() {
+   $("#sportsGifs").empty()
 //"this" refers to the button that was clicked
       var sport = $(this).attr("data-name");
 
@@ -41,36 +40,32 @@ var apiKey = "228z5wIEwb7ZVKcEdK6NJoB2D87Mu7D7";
       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         sport + "&api_key="+ apiKey + "&limit=10";
 
-// Perform the AJAX GET request
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
+	$.ajax({
+	url: queryURL,
+	method: "GET"
+	})
 // After the data comes back from the API
         .done(function(response) {
 // Storing an array of results in the results variable
           var results = response.data;
 
-// Looping over each result item
+// Loop over each result item
           for (var i = 0; i < results.length; i++) {
-
-// Take action if the photo has an appropriate rating
-            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-// Creating a div with the class "item"
+           if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+// Create a div with the class "item"
               var gifDiv = $("<div class='item'>");
-
-// Store the result item's rating
               var rating = results[i].rating;
 
-// Creating a paragraph tag with the result item's rating
               var p = $("<p>").text("Rating: " + rating);
-
-// Creating an image tag
-              var still = results[i].images.fixed_height.url;
-              var animated = results[i].images.fixed_height_still.url;
+              var animated = results[i].images.fixed_height.url;
+              var still = results[i].images.fixed_height_still.url;
               var sportImage = $("<img>");
 // Giving the image tag an src attribute of a property pulled from the result item
-              sportImage.attr("src", results[i].images.fixed_height.url);
+              sportImage.attr("src", still);
+              sportImage.attr("data-still", still);
+              sportImage.attr("data-animated", animated);
+              sportImage.attr("data-state", "still");
+              sportImage.addClass("moveImage");
 
 //Appending the paragraph and sportImage we created to the "gifDiv" div we created
               gifDiv.append(p);
@@ -79,10 +74,20 @@ var apiKey = "228z5wIEwb7ZVKcEdK6NJoB2D87Mu7D7";
 //Prepending the gifDiv to the "#sportsGif" div in the HTML
               $("#sportsGifs").prepend(gifDiv);
             }
-          }
-        });
-    });
+        }
+	});
 
-//Make them still/animated with an on.("click") event
+//Grab the div where the gifs are and their class
+        $("#sportsGifs").on("click", ".moveImage", function(){
+        	var state = $(this).attr("data-state");
+//Check for current state and change it after the click
+        	if(state === 'still'){
+        		$(this).attr('src', $(this).data('animated'));
+        		$(this).attr('data-state', 'animated');
+			}else {
+				$(this).attr('src', $(this).data('still'));
+	       		$(this).attr('data-state', 'still');
+			}
+	  });
+});
 
-//data-still when i hit api, data-animate, data-state, 
